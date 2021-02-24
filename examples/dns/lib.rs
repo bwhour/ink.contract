84796dac0201698eae59b3d4cbfe02fa2414cba6
bwhour <juvemeen.xio@gmail.com>
2021-02-24 10:08:28 +0800

    update some code

--- examples/dns/lib.rs
+++ examples/dns/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Parity Technologies (UK) Ltd.
+// Copyright 2018-2021 Parity Technologies (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -58,8 +58,8 @@ mod dns {
         new_owner: AccountId,
     }
 
-    /// Domain name service contract inspired by ChainX's [blog post]
-    /// (https://medium.com/@chainx_org/secure-and-decentralized-polkadot-domain-name-system-e06c35c2a48d).
+    /// Domain name service contract inspired by ChainX's
+    /// [blog post](https://medium.com/@chainx_org/secure-and-decentralized-polkadot-domain-name-system-e06c35c2a48d).
     ///
     /// # Note
     ///
--- ink.exchange/exchangeproxy/lib.rs
+++ ink.exchange/exchangeproxy/lib.rs
@@ -4,14 +4,37 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod exchangeproxy {
-
+    use token::TokenInterface;
+    use ink_env::call::FromAccountId;
+    use ink_prelude::vec::Vec;
+    use ink_storage::{
+        collections::{HashMap as StorageHashMap, Vec as StorageVec},
+        traits::{PackedLayout, SpreadLayout},
+        Lazy,
+    };
+    #[derive(
+    Debug,
+    Copy,
+    Clone,
+    PartialEq,
+    Eq,
+    scale::Encode,
+    scale::Decode,
+    SpreadLayout,
+    PackedLayout,
+    )]
+    #[cfg_attr(
+    feature = "std",
+    derive(::scale_info::TypeInfo, ::ink_storage::traits::StorageLayout)
+    )]
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
     pub struct Exchangeproxy {
         /// Stores a single `bool` value on the storage.
-        value: bool,
+        _mutex: bool,
+        weth: Laze<TokenInterface>,
     }
     pub struct Swap {
         pool :   AccountId,
@@ -20,44 +43,238 @@ mod exchangeproxy {
         maxPrice:      u32,
     }
 
-    #[ink(event+anonymous)]
-    pub struct LOG_CALL {
+    #[ink(event)]
+    pub struct LOGCALL {
         #[ink(topic)]
-        sig: Option<AccountId>,
+        sig: [u8; 4],
         #[ink(topic)]
         caller: Option<AccountId>,
         data: [u8; 32],
     }
     impl Exchangeproxy {
-
-
-        // pub struct LOG_CALL(
-        // bytes4  indexed sig,
-        // address indexed caller,
-        // bytes          data
-        // )
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(
+            init_value: i32,
+            mutex:bool,
+            weth_code_hash: Hash,
+        ) -> Self {
+            let total_balance = Self::env().balance();
+            let token = TokenInterface::new(init_value)
+                .endowment(total_balance / 4)
+                .code_hash(weth_code_hash)
+                .instantiate()
+                .expect("failed at instantiating the `TokenInterface` contract");
+            Self {
+                _mutex :mutex,
+                weth: Lazy::new(token),
+            }
         }
-
         /// Constructor that initializes the `bool` value to `false`.
         ///
         /// Constructors can delegate to other constructors.
         #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+        pub fn default() -> Self { Self::new(Default::default()) }
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn batchSwapExactIn(
+            &mut self,
+            swaps : Vec<Swap>,
+            tokenIn : AccountId,
+            tokenOut : AccountId,
+            totalAmountIn : u32,
+            minTotalAmountOut : u32,
+        ) -> u32 {
+            // TokenInterface TI = TokenInterface(tokenIn);
+            // TokenInterface TO = TokenInterface(tokenOut);
+            let TI:TokenInterface = FromAccountId::from_account_id(tokenIn);
+            let TO:TokenInterface = FromAccountId::from_account_id(tokenOut);
+            assert_eq!(TI.transfer_from(self.env().caller(),this, totalAmountIn),false);
+            let swap: Vec<_> = swaps.iter().copied().collect();
+            for x in swap.clone().into_iter() {
+                let pool : PoolInterface = PoolInterface::from_account_id(x.pool);
+                if TI.allowance(self.env().account_id(), x.pool) < totalAmountIn {
+                    TI.approve(x.pool, -1);
+                }
+                let tokenAmountOut = pool.swapExactAmountIn(
+                    tokenIn,
+                    x.tokenInParam,
+                    tokenOut,
+                    x.tokenOutParam,
+                    x.maxPrice,
+                );
+                totalAmountOut = add(tokenAmountOut, totalAmountOut);
+            }
+
+            totalAmountOut
         }
 
+
+        #[ink(message)]
+        pub fn batchSwapExactOut (
+            &mut self,
+            swaps : Vec<Swap>,
+            tokenIn : AccountId,
+            tokenOut : AccountId,
+            maxTotalAmountIn : u32,
+        ) -> u32 {
+            // TokenInterface TI = TokenInterface(tokenIn);
+            // TokenInterface TO = TokenInterface(tokenOut);
+            let TI:TokenInterface = FromAccountId::from_account_id(tokenIn);
+            let TO:TokenInterface = FromAccountId::from_account_id(tokenOut);
+            assert_eq!(TI.transfer_from(self.env().caller(),this, maxTotalAmountIn),false);
+            let swap: Vec<_> = swaps.iter().copied().collect();
+            for x in swap.clone().into_iter() {
+                let pool : PoolInterface = PoolInterface::from_account_id(x.pool);
+                if TI.allowance(self.env().account_id(), x.pool) < maxTotalAmountIn {
+                    TI.approve(x.pool, -1);
+                }
+                let tokenAmountIn = pool.swapExactAmountIn(
+                    tokenIn,
+                    x.tokenInParam,
+                    tokenOut,
+                    x.tokenOutParam,
+                    x.maxPrice,
+                );
+                totalAmountIn = add(tokenAmountIn, totalAmountIn);
+            }
+
+            totalAmountIn
+        }
+
+        #[ink(message)]
+        pub fn batchEthInSwapExactIn(
+            &mut self,
+            swaps : Vec<Swap>,
+            tokenOut : AccountId,
+            minTotalAmountOut : u32,
+        ) -> u32 {
+            let TO:TokenInterface = FromAccountId::from_account_id(tokenOut);
+            // weth.deposit.value(self.env().balance())();
+            let swap: Vec<_> = swaps.iter().copied().collect();
+            for x in swap.clone().into_iter() {
+                let pool : PoolInterface = PoolInterface::from_account_id(x.pool);
+                if weth.allowance(self.env().account_id(), x.pool) < self.env().balance() {
+                    weth.approve(x.pool, -1);
+                }
+                let tokenAmountOut = pool.swapExactAmountIn(
+                    weth,
+                    x.tokenInParam,
+                    tokenOut,
+                    x.tokenOutParam,
+                    x.maxPrice,
+                );
+                totalAmountOut = add(tokenAmountOut, totalAmountOut);
+            }
+            let wethBalance = weth.balanceOf(self.env().account_id());
+            if wethBalance > 0 {
+                weth.withdraw(wethBalance);
+                // (bool xfer,) = msg.sender.call.value(wethBalance)("");
+                // require(xfer, "ERR_ETH_FAILED");
+            }
+            totalAmountOut
+        }
+
+        #[ink(message)]
+        pub fn batchEthOutSwapExactIn(
+            &mut self,
+            swaps : Vec<Swap>,
+            tokenIn : AccountId,
+            totalAmountIn : u32,
+            minTotalAmountOut : u32,
+        ) -> u32 {
+            let TI:TokenInterface = FromAccountId::from_account_id(tokenIn);
+            assert_eq!(TI.transfer_from(self.env().caller(),this, totalAmountIn),false);
+            let swap: Vec<_> = swaps.iter().copied().collect();
+            for x in swap.clone().into_iter() {
+                let pool : PoolInterface = PoolInterface::from_account_id(x.pool);
+                if (TI.allowance(self.env().account_id(), x.pool) < maxTotalAmountIn) {
+                    TI.approve(x.pool, -1);
+                }
+                let tokenAmountOut = pool.swapExactAmountIn(
+                    tokenIn,
+                    x.tokenInParam,
+                    weth,
+                    x.tokenOutParam,
+                    x.maxPrice,
+                );
+                totalAmountOut = add(tokenAmountOut, totalAmountOut);
+            }
+            assert!(totalAmountOut>=minTotalAmountOut);
+           let wethBalance = weth.balanceOf(self.env().account_id());
+            weth.withdraw(wethBalance);
+            // (bool xfer,) = msg.sender.call.value(wethBalance)("");
+            // require(xfer, "ERR_ETH_FAILED");
+            assert!(TI.transfer(self.env().caller(),TI.balanceOf(self.env().account_id())));
+            totalAmountOut
+    }
+        #[ink(message)]
+        pub fn batchEthInSwapExactOut(
+            &mut self,
+            swaps : Vec<Swap>,
+            tokenOut : AccountId,
+        ) -> u32 {
+            let TO:TokenInterface = FromAccountId::from_account_id(tokenOut);
+            weth.deposit.value(Default.default());
+            let swap: Vec<_> = swaps.iter().copied().collect();
+            for x in swap.clone().into_iter() {
+                let pool : PoolInterface = PoolInterface::from_account_id(x.pool);
+                if TO.allowance(self.env().account_id(), x.pool) < self.env().balance() {
+                    TO.approve(x.pool, -1);
+                }
+                let tokenAmountIn = pool.swapExactAmountOut(
+                    weth,
+                    x.tokenInParam,
+                    tokenOut,
+                    x.tokenOutParam,
+                    x.maxPrice,
+                );
+                totalAmountIn = add(tokenAmountIn, totalAmountIn);
+                assert_eq!(TO.transfer(self.env().caller(),TO.balanceof(self.env().account_id())),false);
+                let wethBalance = weth.balanceof(self.env().account_id());
+                if wethBalance>0 {
+                    weth.withdraw(wethBalance);
+                    // (bool xfer,) = msg.sender.call.value(wethBalance)("");
+                    // assert_eq!(xfer,false);
+                }
+            }
+            totalAmountIn
+        }
+
+        #[ink(message)]
+        pub fn batchEthOutSwapExactOut(
+            &mut self,
+            swaps : Vec<Swap>,
+            tokenIn : AccountId,
+            maxTotalAmountIn : u32,
+        ) -> u32 {
+            let TI:TokenInterface = FromAccountId::from_account_id(tokenIn);
+            assert_eq!(TI.transfer_from(self.env().caller(),this, totalAmountIn),false);
+            let swap: Vec<_> = swaps.iter().copied().collect();
+            for x in swap.clone().into_iter() {
+                let pool : PoolInterface = PoolInterface::from_account_id(x.pool);
+                if TI.allowance(self.env().account_id(), x.pool) < maxTotalAmountIn {
+                    TI.approve(x.pool, -1);
+                }
+                let tokenAmountIn = pool.swapExactAmountOut(
+                    tokenIn,
+                    x.tokenInParam,
+                    weth,
+                    x.tokenOutParam,
+                    x.maxPrice,
+                );
+                totalAmountIn = add(tokenAmountIn, totalAmountIn);
+            }
+            assert!(maxTotalAmountIn<=maxTotalAmountIn);
+            assert!(TI.transfer(self.env().caller(),TI.balanceOf(self.env().account_id())));
+            let wethBalance = weth.balanceOf(self.env().account_id());
+            weth.withdraw(wethBalance);
+            // (bool xfer,) = msg.sender.call.value(wethBalance)("");
+            // assert!(xfer);
+            totalAmountIn
+        }
+
+
+        //............................................
         /// Simply returns the current value of our `bool`.
         #[ink(message)]
         pub fn get(&self) -> bool {
@@ -68,6 +285,19 @@ mod exchangeproxy {
          let c = a + b;
          //todo assert_eq!(c >= a, "ERR_ADD_OVERFLOW");
          c
+        }
+        fn _logs_(&mut self) {
+            // emit LOG_CALL(msg.sig, msg.sender, msg.data);
+            let caller = self.env().caller();
+            self.env().emit_event();
+        }
+        fn _locks_(&mut self){
+            assert_eq!(self._mutex,false);
+            self._mutex =true;
+        }
+
+        fn _unlocks_(&mut self){
+            self._mutex = false;
         }
     }
 
